25be551ebc01ee1424fcdec3ab4e118fc1ed0a09
sebastian-landl-gofore <sebastian.landl@gofore.com>
2025-08-19 16:38:23 +0200

    Add cosine similarity function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from typing import List, Any
 import json
 import tiktoken
+from sklearn.metrics.pairwise import cosine_similarity
 
 def token_count(text: str, encoding: str = "cl100k_base") -> int:
     encoding = tiktoken.get_encoding(encoding)
@@ -17,3 +18,6 @@ def read_file_lines(filepath: str) -> List[str]:
 def read_json(filepath: str) -> Any:
     with open(filepath, "r") as f:
         return json.load(f)
+
+def cosine_sim(vec1: List[float], vec2: List[float]) -> float:
+    return cosine_similarity([vec1], [vec2]).item()
